cd7968c62dae66a234d034c4092d7b6ba0891bc9
Claudio Pellegrini <ladeclaudio@gmail.com>
2017-05-08 17:02:49 -0300

    commit_tabla_producto

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :productos
   resources :tipos
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'tipos#index'
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170508194640) do
+ActiveRecord::Schema.define(version: 20170508200201) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "productos", force: :cascade do |t|
+    t.string   "nombre"
+    t.text     "descripcion"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "tipos", force: :cascade do |t|
     t.string   "nombre"

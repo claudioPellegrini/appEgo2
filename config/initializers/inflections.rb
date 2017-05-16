03c43ea930daa2d8ee68fe6667796654412875b2
Claudio Pellegrini <ladeclaudio@gmail.com>
2017-05-16 18:47:07 -0300

    nuevas validaciones e idioma

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -7,6 +7,8 @@ ActiveSupport::Inflector.inflections(:en) do |inflect|
 #   inflect.plural /^(ox)$/i, '\1en'
 #   inflect.singular /^(ox)en/i, '\1'
    inflect.irregular 'producto', 'productos'
+   inflect.irregular 'error', 'errores'
+   inflect.irregular 'impide', 'impiden'
 #   inflect.uncountable %w( fish sheep )
 end
 

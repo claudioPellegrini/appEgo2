652a14ad256f3189217b213330c554a4f8490ce0
mecvf <mauge58@gmail.com>
2017-05-15 21:38:14 -0300

    Ahora arregle la macana

--- app/models/fringe.rb
+++
@@ -1,9 +0,0 @@
-class Fringe < ApplicationRecord
-	validates :primera_hasta, presence: true, numericality: true, numericality: { min: 0,less_than: :segunda_hasta }
-	validates :segunda_hasta, presence: true, numericality: true, numericality: { greater_than: :primera_hasta }
-
-  	validates :primera_precio, presence: true, numericality: true, numericality: { min: 0, less_than: :segunda_precio }
-  	validates :segunda_precio, presence: true, numericality: true, numericality: { greater_than_or_equal: :primera_precio, less_than: :tercera_precio }
-
-  	validates :tercera_precio, presence: true, numericality: true, numericality: { greater_than: :segunda_precio }
-end
